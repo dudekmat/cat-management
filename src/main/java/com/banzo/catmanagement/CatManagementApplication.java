f55071bc4f1632a8e9b7eef5b7bc6f7d6b388bed
banzo276 <matdudek2706@gmail.com>
2022-12-06 19:35:02 +0100

    Initial commit

---
+++ src/main/java/com/banzo/catmanagement/CatManagementApplication.java
@@ -0,0 +1,13 @@
+package com.banzo.catmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banzo/catmanagement/CatManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banzo.catmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
